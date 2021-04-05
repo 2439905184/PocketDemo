d01db5c0f7c0bde1ba3ce7adc09c951c93f84d17
2439905184 <suchenglong018@126.com>
2021-04-05 14:59:49 +0800

    备份
    备份

--- test.gd
+++ test.gd
@@ -1,97 +1,37 @@
 extends Node2D
-
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-# warning-ignore:unused_signal
-signal a
-var has
-var _hello
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	has=Engine.has_singleton("HelloGodot")
-	#OS.alert("有单例吗"+str(has))
-	$Label.text+=str(has)
-	_hello=Engine.get_singleton("HelloGodot")
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
-func _on_test_pressed():
-	
-	$Label2.text=str(_hello)
-	#print_tree_pretty()
-	if _hello:
-		OS.alert("有Hello单例")
-#		var array=_hello.get_method_list()
-#		for i in array:
-#			$Label3.text=str(i)
-		#hello.test()
-		var result=_hello.test2()
-		OS.alert(result)
-		pass
-		#hello.connect("on_test",self,"_test")
-	pass # Replace with function body.
-
-func _test():
-	$Label3.text="HelloFromAndroidPlugin"
-	pass
-
-func _on_test2_pressed():
-	if _hello:
-		var t=_hello.test2()
-		OS.alert(t)
-	pass # Replace with function body.
-
-func _on_getGodot_pressed():
-	if _hello:
-		var g=_hello.get_Godot()
-		OS.alert(g)
-	pass # Replace with function body.
-
-
-func _on_Toast_pressed():
-	if _hello:
-		#Toast测试ui线程
-		_hello.test3()
-	pass # Replace with function body.
-
-
-func _on_getActivity_pressed():
-	if _hello:
-		var a=_hello.get_Activity()
-		OS.alert(a)
-	pass # Replace with function body.
-
-
-func _on_getCtx_pressed():
-	if _hello:
-		var ctx=_hello.get_ctx()
-		OS.alert(ctx)
-	pass # Replace with function body.
-
-
-func _on_getGodot2_pressed():
-# warning-ignore:return_value_discarded
-	get_tree().change_scene("res://Yomob.tscn")
-	pass # Replace with function body.
-
-
+var I_has
+var I_aar
 func _on_Button_pressed():
-	var I_has=Engine.has_singleton("StrFromTest")
+	I_has=Engine.has_singleton("StrFromTest")
 	if I_has:
 		OS.alert("存在str from test单利")
-		var I_Strfrom_aar=Engine.get_singleton("StrFromTest")
-		var a=I_Strfrom_aar.getStr()
-		OS.alert(a)
+		I_aar=Engine.get_singleton("StrFromTest")
+		$has.text="has=true"
+		I_aar.connect("adFailed","self","InterAdFailed")
+		#var a=I_aar.getStr()
+		#OS.alert(a)
 	else:
 		OS.alert("不存在单利")
-	
-
-	
 	pass
+
+func InterAdFailed():
+	$state.text="state:failed"
+	pass
+func _on_init_pressed():
+	I_aar.init()
+	pass # Replace with function body.
+
+
+func _on_showad_pressed():
+	I_aar.showAd()
+	pass # Replace with function body.
+
+
+func _on_showBanner_pressed():
+	I_aar.showBanner()
+	pass # Replace with function body.
+
+
+func _on_showNativeAd_pressed():
+	I_aar.showNativeAd()
+	pass # Replace with function body.
